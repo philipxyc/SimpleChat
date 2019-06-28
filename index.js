cc127e21a84d81a8ed27643a1b3ebbb3a45b930f
xiayc <philipxyc@live.com>
2019-06-28 19:11:11 +0800

    half way to add identification

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 var express = require('express')
 var app = express();
 var http = require('http').Server(app);
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
+
+db.serialize(function() {
+  db.run("CREATE TABLE ROOM (ID TEXT PRIMARY KEY NOT NULL, ALLOW INT)");
+
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+});
+
+// db.close();
+
+
 var io = require('socket.io')(http);
 
 app.use(express.static('public'));
@@ -11,21 +28,26 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  socket.on('join', function(rid){
+    console.log('user join: ' + msg);
+
+  });
+  socket.on('create', function(msg){
+    console.log('user create');
+    if(msg.hasOwnProperty('rid') && msg.hasOwnProperty('limit')){
+
+      db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+          console.log(row.id + ": " + row.info);
+      });
+      io.emit(msg.rid, 'created');
+    }
+
+  });
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-  socket.on('reset', function(){
-    console.log('reset');
-    io.emit('reset');
-  });
-  socket.on('player0', function(msg){
-    console.log('player0: ' + msg);
-    io.emit('player0', msg);
-  });
-  socket.on('player1', function(msg){
-    console.log('player1: ' + msg);
-    io.emit('player1', msg);
-  });
+
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message', msg);
